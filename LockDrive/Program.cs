8c7fb6a7279b7e20aa0090fb4c7529a93b978346
soukoku <yinchun.wang@gmail.com>
2015-01-19 22:02:34 -0500

    First checkin

---
+++ LockDrive/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace LockDrive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maybeDrive = args.FirstOrDefault();
+            if (string.IsNullOrEmpty(maybeDrive))
+            {
+                Console.WriteLine("==================================================");
+                Console.WriteLine("This utility locks an unlocked BitLocker drive.");
+                Console.WriteLine("Pass the drive letter as the parameter or type it");
+                Console.WriteLine("below to use it.");
+                Console.WriteLine("==================================================");
+                Console.WriteLine();
+                Console.Write("Enter the drive letter (or enter to exit):");
+
+                maybeDrive = Console.ReadLine();
+            }
+
+            if (!string.IsNullOrEmpty(maybeDrive) &&
+                CheckAndRestartAsAdmin(maybeDrive))
+            {
+                if (maybeDrive.Length == 1)
+                {
+                    maybeDrive += ':';
+                }
+                try
+                {
+                    var drive = new DirectoryInfo(maybeDrive.Trim(' ', '\"'));
+                    if (drive.Parent == null)
+                    {
+                        TryUnUnlockDrive(drive);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                    Console.WriteLine();
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static void TryUnUnlockDrive(DirectoryInfo drive)
+        {
+            var path = new ManagementPath
+            {
+                NamespacePath = "\\ROOT\\CIMV2\\Security\\MicrosoftVolumeEncryption",
+                ClassName = "Win32_EncryptableVolume"
+            };
+
+            using (var wmiClass = new ManagementClass(path))
+            {
+                foreach (ManagementObject vol in wmiClass.GetInstances())
+                {
+                    var letter = vol["DriveLetter"].ToString();
+                    if (drive.Name.StartsWith(letter, StringComparison.OrdinalIgnoreCase))
+                    {
+                        var status = (uint)vol["ProtectionStatus"];
+                        if (status == 1)
+                        {
+                            var inParams = vol.GetMethodParameters("Lock");
+                            inParams["ForceDismount"] = false;
+                            var outParams = vol.InvokeMethod("Lock", inParams, null);
+                            var result = (uint)outParams["returnValue"];
+                            switch (result)
+                            {
+                                case 0://S_OK
+                                    return;
+                                case 0x80070005: // E_ACCESS_DENIED
+                                    throw new Exception("Access denied.");
+                                case 0x80310001: // FVE_E_NOT_ENCRYPTED
+                                    throw new Exception("Not encrypted.");
+                                case 0x80310021: // FVE_E_PROTECTION_DISABLED
+                                    throw new Exception("Protection disabled.");
+                                case 0x80310022: // FVE_E_RECOVERY_KEY_REQUIRED
+                                    throw new Exception("Key required.");
+                                default:
+                                    throw new Exception(string.Format("Unknown code {0:X}", result));
+                            }
+                        }
+                        else
+                        {
+                            throw new Exception("Not an unlocked BitLocker drive.");
+                        }
+                    }
+                }
+            }
+            throw new Exception(string.Format("No drive found for {0}.", drive.Name));
+        }
+
+        private static bool CheckAndRestartAsAdmin(string arg)
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            if (principal.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                return true;
+            }
+            else
+            {
+                var startInfo = new ProcessStartInfo(Assembly.GetEntryAssembly().Location);
+                startInfo.Arguments = arg;
+                startInfo.Verb = "runas";
+                Process.Start(startInfo);
+            }
+            return false;
+        }
+    }
+}
---
+++ LockDrive/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LockDrive")]
+[assembly: AssemblyDescription("Locks a bitlocker drive if it's passed as the first command parameter.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Eugene Wang")]
+[assembly: AssemblyProduct("LockDrive")]
+[assembly: AssemblyCopyright("Copyright © Eugene Wang 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9b03eb17-e49b-4376-b0fa-1e0e69a69b2c")]
+
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
